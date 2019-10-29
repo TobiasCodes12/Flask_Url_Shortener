ad5f5ae62f00adca44aedf8616ce2b13d9b5df19
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-10-29 08:30:55 -0400

    Add link_added.html, database tables

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -5,7 +5,7 @@ from .extensions import db
 
 class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    original_url = db.Column(db.String(500))
+    original_url = db.Column(db.String(512))
     shorten_url = db.Column(db.String(14), unique=True)
     visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
@@ -13,11 +13,11 @@ class Link(db.Model):
     #  inheritance
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.shorten_url = self.create_short_link
+        self.shorten_url = self.create_short_link()
 
     def create_short_link(self):
         characters = string.digits + string.ascii_letters 
-        shorten_url = ''.join(choices(characters, k=3))
+        shorten_url = ''.join(choices(characters, k=14))
 
         link = self.query.filter_by(shorten_url=shorten_url).first()
 
--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -1,6 +1,7 @@
 import os 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 ADMIN_USERNAME = os.environ.get('ADMIN_UNAME')
